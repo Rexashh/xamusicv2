8c920b3b4d46181a5be02c0ff9e1dfc47a649523
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-04-03 18:43:37 +0700

    hmm

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Also without customizing or modifying as your own choice, this must be
 # filled with Yukki Music Bot Main Repository URL in value.
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/Rexashh/Yukki"
+    "UPSTREAM_REPO", "https://github.com/Rexashh/yukkixa"
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
